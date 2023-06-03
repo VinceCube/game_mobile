81d71ae63ab68c506a4691a1433673ce163dc280
VinceCube <130943095+VinceCube@users.noreply.github.com>
2023-06-03 14:47:25 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,6 +6,25 @@
              .register('./sw.js');
   }
 }*/
+window.addEventListener('beforeinstallprompt', function(event) {
+  event.preventDefault(); // Prevent automatic prompt
+  var deferredPrompt = event;
+  
+  // Show your custom "Add to Home Screen" button or UI
+  myAddToHomeScreenButton.addEventListener('click', function() {
+    deferredPrompt.prompt(); // Show installation prompt
+    deferredPrompt.userChoice.then(function(choiceResult) {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null; // Reset the deferred prompt
+    });
+  });
+});
+
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('/service-worker.js')
